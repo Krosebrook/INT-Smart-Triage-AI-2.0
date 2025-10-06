b272910562e70091373ea3b7dbc7c0840a245c0d
Kyle R <214532761+Krosebrook@users.noreply.github.com>
2025-10-06 17:02:48 -0500

    Updated package-lock.json

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: '.',
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      }
+    }
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  }
+});
